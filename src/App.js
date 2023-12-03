52ed83d1e2b77018a975c6cbfb2fd687b0ad1928
Nathan Towe <nathanwtowe89@gmail.com>
2023-12-03 03:50:51 -0500

    started the navbar and inserted img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Navbar bg="light" expand="lg" >
+      <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src="https://www.instacart.com/assets/beetstrap/brand/2022/instacart-logo-color-6678cb82d531f8910d5ba270a11a7e9b56fc261371bda42ea7a5abeff3492e1c.svg" alt="Instacart Logo"/> 
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
